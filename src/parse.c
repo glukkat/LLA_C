9b080f9f21c8eee9a0d006401baa0df5f8a6de8b
glukkat <glukkat@proton.me>
2026-01-06 20:08:54 +0100

    Update parse.c

--- src/parse.c
+++ src/parse.c
@@ -24,7 +24,7 @@ int read_employees(int fd, struct dbheader_t *dbhdr, struct employee_t **employe
 	//
 }
 */
-int output_file(int fd, struct dbheader_t *dbhdr/*, struct employee_t *employees*/) {
+int output_file(int fd, struct dbheader_t *dbhdr, struct employee_t *employees) {
 
 	if (fd < 0) {
 		printf("FATAL ERROR: Invalid File descriptor.");
@@ -87,7 +87,7 @@ int validate_db_header(int fd, struct dbheader_t **headerOut) {
 	*headerOut = header;
 }
 
-int create_db_header(int fd, struct dbheader_t **headerOut) {
+int create_db_header(struct dbheader_t **headerOut) {
 
 	struct dbheader_t*header = calloc (1, sizeof(struct dbheader_t));
 	if (header == NULL) {
